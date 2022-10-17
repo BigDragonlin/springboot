e57e9712687796c133662cc361602c6ea7a09e52
bigdragon <bigdragonlin@qq.com>
2022-10-17 22:37:59 +0800

    springSecurity

---
+++ springsecutity-01-hello/src/test/java/com/bg/springsecutity01hello/Springsecutity01HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bg.springsecutity01hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecutity01HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
